734793a53edde854ff0eceaf24a1be37aaa4a0e2
Heutlett <adrian.araya@estudiantec.cr>
2020-02-29 23:15:15 -0600

    Se agregan funciones nuevas a la estructura lista

---
+++ Tarea 1/linked_list.h
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   linked_list.h
+ * Author: heutlett
+ *
+ * Created on February 29, 2020, 10:52 PM
+ */
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+private:
+    node *head,*tail;
+public:
+    linked_list();
+
+    void add_node(int n);
+
+    node* gethead();
+
+    void imprimir();
+
+    void front(int n);
+
+    void after(node *a, int value);
+    
+    int getSize();
+};
+
+#endif /* LINKED_LIST_H */
+
