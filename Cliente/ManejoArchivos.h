3602cfb5d7e40e0f193339430f7bb4d2794bb080
Heutlett <adrian.araya@estudiantec.cr>
2020-03-01 19:12:29 -0600

    Se agrega la funcionalidad de mandar strings en sockets

---
+++ Cliente/ManejoArchivos.h
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ManejoArchivos.h
+ * Author: heutlett
+ *
+ * Created on March 1, 2020, 6:33 PM
+ */
+
+#ifndef MANEJOARCHIVOS_H
+#define MANEJOARCHIVOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+class ManejoArchivos {
+public:
+    ManejoArchivos();
+    
+    string abrirGrafoTXT(string txt){
+        
+    fstream archivo;
+    string texto;
+    string texto2 = "";
+    
+    archivo.open(txt, ios::in);
+    
+    if(archivo.fail()){
+        
+        printf("No se pudo abrir el archivo");
+        exit(1);
+        
+    }
+    
+    while(!archivo.eof()){
+        
+        getline(archivo, texto);
+        texto2 += texto + "\n";
+        
+    }
+    
+    
+    
+    return texto2;
+        
+    }
+
+private:
+
+};
+
+#endif /* MANEJOARCHIVOS_H */
+
---
+++ Cliente/client.cpp
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Client side C/C++ program to demonstrate Socket programming 
+
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include "ManejoArchivos.h"
+#define PORT 8081 
+
+int main(int argc, char const *argv[]) 
+{ 
+    ManejoArchivos * a;
+    
+    string mensaje = a->abrirGrafoTXT("texto.txt");
+    
+    
+	int sock = 0, valread; 
+	struct sockaddr_in serv_addr; 
+	char *hello = "Hello from client"; 
+        //char *hello = a->abrirGrafoTXT("texto.txt");
+	char buffer[1024] = {0}; 
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	} 
+	send(sock , mensaje.data() , mensaje.size() , 0 ); 
+	printf("Hello message sent\n"); 
+	valread = read( sock , buffer, 1024); 
+	printf("%s\n",buffer ); 
+	return 0; 
+} 
